8df83a02cc7bbf60da35f1edd0243cdeab8925a0
EVMihaylov <eugene.mikhailow@gmail.com>
2022-03-17 22:19:27 +0300

    Homework 2 10

--- cart-service/src/main/java/com/geekbrains/spring/web/cart/exceptions/GlobalExceptionHandler.java
+++ cart-service/src/main/java/com/geekbrains/spring/web/cart/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.geekbrains.spring.web.cart.exceptions;
 
-import com.geekbrains.spring.web.api.exceptions.AppError;
-import com.geekbrains.spring.web.api.exceptions.InternalServerErrorException;
-import com.geekbrains.spring.web.api.exceptions.ResourceNotFoundException;
-import com.geekbrains.spring.web.api.exceptions.UserAlreadyExistsException;
+import com.geekbrains.spring.web.api.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,4 +12,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<AppError> catchResourceNotFoundException(ResourceNotFoundException e){
         return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
     }
+    @ExceptionHandler
+    public ResponseEntity<AppError> catchCoreServiceNotWorkingException(CoreServiceNotWorkingException e){
+        return new ResponseEntity<>(new AppError(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
--- cart-service/src/main/java/com/geekbrains/spring/web/cart/integration/ProductsServiceIntegration.java
+++ cart-service/src/main/java/com/geekbrains/spring/web/cart/integration/ProductsServiceIntegration.java
@@ -1,25 +1,32 @@
 package com.geekbrains.spring.web.cart.integration;
 
+import com.geekbrains.spring.web.api.carts.CartDto;
 import com.geekbrains.spring.web.api.core.ProductDto;
+import com.geekbrains.spring.web.api.exceptions.CoreServiceNotWorkingException;
+import com.geekbrains.spring.web.api.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ProductsServiceIntegration {
-    private final RestTemplate restTemplate;
-
-    @Value("${integrations.core-service.url}")
-    private String productServiceUrl;
+    private final WebClient coreServiceWebClient;
 
     public Optional<ProductDto> findById(Long id){
-        ProductDto productDto = restTemplate.getForObject(productServiceUrl + "/api/v1/products/" + id, ProductDto.class);
+        ProductDto productDto = coreServiceWebClient.get()
+                .uri("/api/v1/products/" + id)
+                .retrieve()
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new CoreServiceNotWorkingException("Product service is not working")))
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new ResourceNotFoundException(String.format("Product with id %d is not found", id))))
+                .bodyToMono(ProductDto.class)
+                .block();
         return Optional.ofNullable(productDto);
     }
-
-
 }
